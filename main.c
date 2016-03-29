3e375bc9eaf3c401bf616f56cacbe111c330ae4b
Nick Ashley <nick@shley.us>
2016-03-29 12:27:58 -0400

    Attempt to get pixel colors
    Not working yet; the colors don't appear to be right, RGBA seems to be BGRA, and the program segfaults

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include <sys/mman.h>	// memory management
 #include <unistd.h>		// close()
 #include <sys/ioctl.h>
+#include <sys/kd.h>		//KDSETMODE; set mode of vt
 
 int main(int argc, int **argv){
 	int sf;
@@ -15,6 +16,14 @@ int main(int argc, int **argv){
 	struct fb_fix_screeninfo finfo;
 	long int screensize = 0;
 	char *fbp = 0;
+	unsigned long *kb_mode; // save the keyboard mode of the tty
+	long int location = 0;
+
+	int fd = 1;
+
+	//ioctl(fd, KDSETMODE, KD_GRAPHICS);
+	ioctl(fd, KDGKBMODE, kb_mode);
+	//ioctl(fd, KDSKBMODE, K_RAW);
 
 	sf = open("/dev/fb0",O_RDONLY); // open framebuffer device as readonly
 	if(sf == -1)
@@ -31,12 +40,29 @@ int main(int argc, int **argv){
 	// Figure out the size of the screen in bytes
     screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
 
+    printf("Screen size: %d\n", screensize);
+
     fbp = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, sf, 0);
 
     if(fbp == NULL)
     	perror("Error mapping the framebuffer device to memory");
 
+    //int bytespp = (vinfo.bits_per_pixel / 8);
+    int bytespp = 1;
+    
+    printf("FBP:0x%x xoffset:%d yoffset:%d bytespp:%d line_length:%d\n", &fbp, vinfo.xoffset, vinfo.yoffset, bytespp, finfo.line_length);
+
+    for(int y = 0; y < vinfo.yres; y++)
+    	for(int x = 0; x < vinfo.xres; x++){
+    		location = (x + vinfo.xoffset) * bytespp + (y + vinfo.yoffset) * (finfo.line_length);
+    		//*(fbp + location + 0) = 100;
+    		printf("(x=%4d,y=%4d) Location: %lu BGRA: (%u,%u,%u,%u)\n", x, y, location, (u_char)*(&fbp+location+0), (u_char)*(&fbp+location+1), (u_char)*(&fbp+location+2), (u_char)*(&fbp+location+3));
+    		//printf("(x=%d,y=%d) Location: %lu BGRA: (%d,%d,%d)\n", x, y, location, *(&fbp+location+0), *(&fbp+location+1), *(&fbp+location+2), *(&fbp+location+3));
+    	}
+
     munmap(fbp, screensize);
+    //ioctl(fd, KDSETMODE, KD_TEXT);
+    //ioctl(fd, KDSKBMODE, kb_mode);
     close(sf);
 	return 0;
 }
