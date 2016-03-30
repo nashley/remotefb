fa8fcff8d56341462bca01ef7a72e20ca2035f81
Nick Ashley <nick@shley.us>
2016-03-30 09:57:25 -0400

    Added main loop

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
-#include <stdlib.h>		// standard library utils
-#include <stdio.h>		// standard I/O
+#include <stdlib.h>     // standard library utils
+#include <stdio.h>      // standard I/O
 
-#include <linux/fb.h>	// framebuffer
+#include <linux/fb.h>   // framebuffer
 
-//#include <sys/stat.h>	//
-#include <fcntl.h>		// file opening
-#include <sys/mman.h>	// memory management
-#include <unistd.h>		// close()
+//#include <sys/stat.h> //
+#include <fcntl.h>      // file opening
+#include <sys/mman.h>   // memory management
+#include <unistd.h>     // close()
 #include <sys/ioctl.h>
-#include <sys/kd.h>		// KDSETMODE; set mode of vt
-#include <stdint.h>		// uint8_t
-#include <time.h>		// time
-#include <string.h>		// memcmp
+#include <sys/kd.h>     // KDSETMODE; set mode of vt
+#include <stdint.h>     // uint8_t
+#include <time.h>       // time
+#include <string.h>     // memcmp
 
 void get_screenshot(struct fb_var_screeninfo *vinfo, struct fb_fix_screeninfo *finfo);
 
@@ -26,31 +26,31 @@ int d;
 int bytespp;
 
 int main(int argc, int **argv){
-	int sf;
-	struct fb_var_screeninfo vinfo;
-	struct fb_fix_screeninfo finfo;
+    int sf;
+    struct fb_var_screeninfo vinfo;
+    struct fb_fix_screeninfo finfo;
 
-	unsigned long *kb_mode; // save the keyboard mode of the tty
+    unsigned long *kb_mode; // save the keyboard mode of the tty
 
-	int fd = 1;
+    int fd = 1;
 
-	//ioctl(fd, KDSETMODE, KD_GRAPHICS);
-	ioctl(fd, KDGKBMODE, kb_mode);
-	//ioctl(fd, KDSKBMODE, K_RAW);
-	printf("\n");
-	sf = open("/dev/fb0",O_RDWR); // open framebuffer device as readonly
-	if(sf == -1)
-		perror("There was an error opening the framebuffer device. Ensure that you have the required permissions (e.g., video group)");
-	
-	if (ioctl(sf, FBIOGET_FSCREENINFO, &finfo) == -1)
-		perror("There was an error getting the fixed video information");
+    //ioctl(fd, KDSETMODE, KD_GRAPHICS);
+    ioctl(fd, KDGKBMODE, kb_mode);
+    //ioctl(fd, KDSKBMODE, K_RAW);
+    printf("\n");
+    sf = open("/dev/fb0",O_RDWR); // open framebuffer device as readonly
+    if(sf == -1)
+        perror("There was an error opening the framebuffer device. Ensure that you have the required permissions (e.g., video group)");
+    
+    if (ioctl(sf, FBIOGET_FSCREENINFO, &finfo) == -1)
+        perror("There was an error getting the fixed video information");
 
-	if (ioctl(sf, FBIOGET_VSCREENINFO, &vinfo) == -1)
-		perror("There was an error getting the variable video information");
+    if (ioctl(sf, FBIOGET_VSCREENINFO, &vinfo) == -1)
+        perror("There was an error getting the variable video information");
 
-	printf("%dx%d, %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel); // Framebuffer resolution and bits per pixel
+    printf("%dx%d, %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel); // Framebuffer resolution and bits per pixel
 
-	// Figure out the size of the screen in bytes
+    // Figure out the size of the screen in bytes
     //screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
     screensize = finfo.smem_len;
     //screensize = vinfo.yres_virtual * finfo.line_length;
@@ -61,7 +61,7 @@ int main(int argc, int **argv){
     previous_buffer = malloc(screensize);
 
     if(fbp == NULL)
-    	perror("Error mapping the framebuffer device to memory");
+        perror("Error mapping the framebuffer device to memory");
 
     bytespp = (vinfo.bits_per_pixel / 8);
     //int bytespp = 1;
@@ -71,46 +71,43 @@ int main(int argc, int **argv){
 
     void *previous_buffer;
 
-    clock_t tic = clock();
+    int i = 0;
 
-    get_screenshot(&vinfo, &finfo);
+    while(1){
 
-    printf("First Screenshot differences: %d\n", d);
+        clock_t tic = clock();
 
-    clock_t toc = clock();
+        get_screenshot(&vinfo, &finfo);
 
-    get_screenshot(&vinfo, &finfo);
+        clock_t toc = clock();
 
-    clock_t tac = clock();
+        printf("Screenshot #%2d differences: %d took %fs\n", i, d, (double)(toc-tic)/CLOCKS_PER_SEC);
 
-    //get_screenshot(&vinfo, &finfo);get_screenshot(&vinfo, &finfo);get_screenshot(&vinfo, &finfo);get_screenshot(&vinfo, &finfo);get_screenshot(&vinfo, &finfo);get_screenshot(&vinfo, &finfo);get_screenshot(&vinfo, &finfo);get_screenshot(&vinfo, &finfo);get_screenshot(&vinfo, &finfo);
-
-    printf("Screenshot time 1: %f\n", (double)(toc-tic)/CLOCKS_PER_SEC);
-    printf("Screenshot time 2: %f\n", (double)(tac-toc)/CLOCKS_PER_SEC);
-    printf("Second Screenshot differences: %d\n", d);
+        i++;
+    }
 
     munmap(fbp, screensize);
     //ioctl(fd, KDSETMODE, KD_TEXT);
     //ioctl(fd, KDSKBMODE, kb_mode);
     close(sf);
-	return 0;
+    return 0;
 }
 
 void get_screenshot(struct fb_var_screeninfo *vinfo, struct fb_fix_screeninfo *finfo){
-	d = 0;
-	for(int x = 0; x < vinfo->xres; x++)
-    	for(int y = 0; y < vinfo->yres; y++){
-    		location = (x + vinfo->xoffset) * bytespp + (y + vinfo->yoffset) * finfo->line_length;
-			if(*((uint32_t*)(fbp+location)) != *((uint32_t*)(previous_buffer+location))){
-	    		*((uint32_t*)(previous_buffer+location)) = *((uint32_t*)(fbp+location));
-	    		d++;
-			 //if(__builtin_expect(memcmp(*(fbp+location), *(previous_buffer+location)/* || *((uint8_t*)(fbp+location+1)) ^ *((uint8_t*)(previous_buffer+location+1)) ^ *((uint8_t*)(fbp+location+2)) != *((uint8_t*)(previous_buffer+location+2))*/,uint8_t) != 0, 0)){
-     			//printf("Different @ %ld!\n", location);
-     		}
-    		//printf("Location: %ld\n", location);
-    		//*(fbp + location + 0) = 100;
-    		//printf("(x=%4d,y=%4d) Location: %ld BGR: (%hhu,%hhu,%hhu : %hhu,%hhu,%hhu)\n", x, y, location, *((uint8_t*)(fbp+location+0)), *((uint8_t*)(fbp+location+1)), *((uint8_t*)(fbp+location+2)) , *((uint8_t*)(previous_buffer+location+0)), *((uint8_t*)(previous_buffer+location+1)), *((uint8_t*)(previous_buffer+location+2)) );
-    		//printf("(x=%4d,y=%4d) BGR: (%u)\n", x, y, *((uint32_t*)(fbp+location)));
-    		//printf("(x=%d,y=%d) Location: %lu BGRA: (%d,%d,%d)\n", x, y, location, *(&fbp+location+0), *(&fbp+location+1), *(&fbp+location+2), *(&fbp+location+3));
-    	}
+    d = 0;
+    for(int x = 0; x < vinfo->xres; x++)
+        for(int y = 0; y < vinfo->yres; y++){
+            location = (x + vinfo->xoffset) * bytespp + (y + vinfo->yoffset) * finfo->line_length;
+            if(*((uint32_t*)(fbp+location)) != *((uint32_t*)(previous_buffer+location))){
+                *((uint32_t*)(previous_buffer+location)) = *((uint32_t*)(fbp+location));
+                d++;
+             //if(__builtin_expect(memcmp(*(fbp+location), *(previous_buffer+location)/* || *((uint8_t*)(fbp+location+1)) ^ *((uint8_t*)(previous_buffer+location+1)) ^ *((uint8_t*)(fbp+location+2)) != *((uint8_t*)(previous_buffer+location+2))*/,uint8_t) != 0, 0)){
+                //printf("Different @ %ld!\n", location);
+            }
+            //printf("Location: %ld\n", location);
+            //*(fbp + location + 0) = 100;
+            //printf("(x=%4d,y=%4d) Location: %ld BGR: (%hhu,%hhu,%hhu : %hhu,%hhu,%hhu)\n", x, y, location, *((uint8_t*)(fbp+location+0)), *((uint8_t*)(fbp+location+1)), *((uint8_t*)(fbp+location+2)) , *((uint8_t*)(previous_buffer+location+0)), *((uint8_t*)(previous_buffer+location+1)), *((uint8_t*)(previous_buffer+location+2)) );
+            //printf("(x=%4d,y=%4d) BGR: (%u)\n", x, y, *((uint32_t*)(fbp+location)));
+            //printf("(x=%d,y=%d) Location: %lu BGRA: (%d,%d,%d)\n", x, y, location, *(&fbp+location+0), *(&fbp+location+1), *(&fbp+location+2), *(&fbp+location+3));
+        }
 }
