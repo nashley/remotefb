76e8837c0456b54e63dfef9ff5acc919c7498099
Nick Ashley <nick@shley.us>
2016-04-01 17:02:50 -0400

    Actually go through all the pixels

--- main.c
+++ main.c
@@ -14,6 +14,7 @@
 #include <string.h>     // memcmp
 
 void get_screenshot(struct fb_var_screeninfo *vinfo, struct fb_fix_screeninfo *finfo);
+void write_screenshot(struct fb_var_screeninfo *vinfo, struct fb_fix_screeninfo *finfo);
 
 static uint8_t *previous_buffer;
 static uint8_t *fbp;
@@ -21,12 +22,15 @@ static uint8_t *fbp;
 static long int location = 0;
 long int screensize = 0;
 
-int xoff;
-int yoff;
+int off;
 
 int d;
+int d1;
 
 int bytespp;
+
+int xmax;
+int ymax;
 
 int main(int argc, int **argv){
     int sf;
@@ -62,6 +66,7 @@ int main(int argc, int **argv){
 
     fbp = mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, sf, (off_t)0);
     previous_buffer = malloc(screensize);
+    memset(previous_buffer, 0, screensize);
 
     if(fbp == NULL)
         perror("Error mapping the framebuffer device to memory");
@@ -76,9 +81,12 @@ int main(int argc, int **argv){
 
     int i = 0;
 
-    xoff = vinfo.xoffset * finfo.line_length;
-    yoff = vinfo.yoffset * finfo.line_length;
+    int xoff = vinfo.xoffset * bytespp;
+    int yoff = vinfo.yoffset * finfo.line_length;
+    off = xoff + yoff;
 
+    xmax = vinfo.xres * bytespp;
+    ymax = vinfo.yres * finfo.line_length;
     while(1){
 
         clock_t tic = clock();
@@ -87,7 +95,16 @@ int main(int argc, int **argv){
 
         clock_t toc = clock();
 
+        clock_t tac = clock();
+
+        write_screenshot(&vinfo, &finfo);
+
+        clock_t tuc = clock();
+
+        //sleep(3);
+
         printf("Screenshot #%2d differences: %d took %fs\n", i, d, (double)(toc-tic)/CLOCKS_PER_SEC);
+        printf("Write #%2d differences: %d took %fs\n", i, d1, (double)(toc-tic)/CLOCKS_PER_SEC);
 
         i++;
     }
@@ -101,22 +118,33 @@ int main(int argc, int **argv){
 
 void get_screenshot(struct fb_var_screeninfo *vinfo, struct fb_fix_screeninfo *finfo){
     d = 0;
-    //printf("xres:%d yres:%d xd:%d yd: %d\n", vinfo->xres, vinfo->yres, bytespp * finfo->line_length);
-    for(int x = 0; x < vinfo->xres; x+=bytespp)
-        for(int y = 0; y < vinfo->yres; y+=bytespp * finfo->line_length){
+    printf("xres:%d yres:%d xd:%d yd: %d\n", vinfo->xres, vinfo->yres, bytespp * finfo->line_length);
+    for(int x = 0; x < xmax; x+=bytespp)
+        for(int y = 0; y < ymax; y+=finfo->line_length){
             //printf("xoff: %d yoff:%d x:%d y:%d\n", xoff, yoff, x, y);
-            location = (x + xoff) + (y + yoff);
-            printf("Location: %lu\n", location);
+            location = x + y + off;
+            //printf("Location: %lu\n", location);
             if(*((uint32_t*)(fbp+location)) != *((uint32_t*)(previous_buffer+location))){
-                memcpy((previous_buffer+location),(fbp+location), sizeof(uint32_t));
+                memcpy((previous_buffer+location),(fbp+location), 4); // sizeof(uint32_t)
                 d++;
             }
-             //if(__builtin_expect(memcmp(*(fbp+location), *(previous_buffer+location)/* || *((uint8_t*)(fbp+location+1)) ^ *((uint8_t*)(previous_buffer+location+1)) ^ *((uint8_t*)(fbp+location+2)) != *((uint8_t*)(previous_buffer+location+2))*/,uint8_t) != 0, 0)){
-                //printf("Different @ %ld!\n", location);
-            //printf("Location: %ld\n", location);
-            //*(fbp + location + 0) = 100;
-            //printf("(x=%4d,y=%4d) Location: %ld BGR: (%hhu,%hhu,%hhu : %hhu,%hhu,%hhu)\n", x, y, location, *((uint8_t*)(fbp+location+0)), *((uint8_t*)(fbp+location+1)), *((uint8_t*)(fbp+location+2)) , *((uint8_t*)(previous_buffer+location+0)), *((uint8_t*)(previous_buffer+location+1)), *((uint8_t*)(previous_buffer+location+2)) );
-            //printf("(x=%4d,y=%4d) BGR: (%u)\n", x, y, *((uint32_t*)(fbp+location)));
-            //printf("(x=%d,y=%d) Location: %lu BGRA: (%d,%d,%d)\n", x, y, location, *(&fbp+location+0), *(&fbp+location+1), *(&fbp+location+2), *(&fbp+location+3));
+            //printf("Location0: %lu\n", location);
         }
+    printf("Location1: %lu\n", location);
+}
+void write_screenshot(struct fb_var_screeninfo *vinfo, struct fb_fix_screeninfo *finfo){
+    d1 = 0;
+    //memset(fbp, 0, screensize);
+    //sleep(1);
+    for(int x = 0; x < vinfo->xres * bytespp; x+=bytespp)
+        for(int y = 0; y < vinfo->yres * finfo->line_length; y+=finfo->line_length){
+            //printf("xoff: %d yoff:%d x:%d y:%d\n", xoff, yoff, x, y);
+            location = x + y + off;
+            //printf("Location: %lu\n", location);
+            if(*((uint32_t*)(fbp+location)) != *((uint32_t*)(previous_buffer+location))){
+                memcpy((fbp+location), (previous_buffer+location), 4); // sizeof(uint32_t)
+                d1++;
+            }
+        }
+    //sleep(1);
 }
